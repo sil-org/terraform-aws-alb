dcb758011ede021b181ed43cf87bf7c53adbbee9
briskt <3172830+briskt@users.noreply.github.com>
2026-07-06 12:35:39 -0600

    enable shared workflow; add variable types

--- variables.tf
+++ variables.tf
@@ -61,42 +61,57 @@ variable "tg_name" {
 }
 
 variable "health_check_interval" {
-  default = "30"
+  description = "Approximate amount of time, in seconds, between health checks of an individual target."
+  type        = number
+  default     = 30
 }
 
 variable "health_check_path" {
+  type    = string
   default = "/"
 }
 
 variable "health_check_port" {
+  type    = string
   default = "traffic-port"
 }
 
 variable "health_check_protocol" {
+  type    = string
   default = "HTTP"
 }
 
 variable "health_check_timeout" {
-  default = "5"
+  description = "Amount of time, in seconds, during which no response from a target means a failed health check."
+  type        = number
+  default     = 5
 }
 
 variable "healthy_threshold" {
-  default = "5"
+  description = "Number of consecutive health check successes required before considering a target healthy."
+  type        = number
+  default     = 5
 }
 
 variable "unhealthy_threshold" {
-  default = "2"
+  description = "Number of consecutive health check failures required before considering a target unhealthy."
+  type        = number
+  default     = 2
 }
 
 variable "health_check_status_codes" {
+  type    = string
   default = "200"
 }
 
 variable "idle_timeout" {
-  default = "60"
+  description = "Time in seconds that the connection is allowed to be idle."
+  type        = number
+  default     = 60
 }
 
 variable "load_balancer_type" {
+  type    = string
   default = "application"
 }
 
@@ -107,7 +122,10 @@ variable "enable_ipv6" {
 }
 
 variable "disable_public_ipv4" {
-  description = "false: use public IPv4; true: if enable_ipv6 is true, ip_address_type will be \"dualstack-without-public-ipv4\", otherwise, \"dualstack\""
+  description = <<-EOT
+    false: use public IPv4; true: if enable_ipv6 is true, ip_address_type will be "dualstack-without-public-ipv4",
+    otherwise, "dualstack"
+  EOT
   type        = bool
   default     = false
 }
